2d81a65ccbb11913763aab62ca90caffc083dfb9
Luan Pablo <luanpab@gmail.com>
2019-12-30 21:44:51 -0500

    Add sanitized data

--- src/sanitizers/popular-given-names.js
+++ src/sanitizers/popular-given-names.js
@@ -1,7 +1,35 @@
 const Sanitizer = () => {}
 
 Sanitizer.sanitize = (data) => {
-  return data
+  const result = []
+
+  for (obj of data) {
+    obj.region = obj.region
+      .replace(/&#[0-9]+;([0-9]+)?/gim, '')
+      .replace('citation needed', '')
+      .trim()
+
+    obj.name = obj.name.trim()
+    if (obj.name === 'NA') {
+      continue
+    }
+
+    // split and see if there's more than one name on the object
+
+    const names = obj.name.split(',')
+    if (names.length === 1) {
+      result.push(obj)
+      continue
+    }
+
+    for (name of names) {
+      const _obj = Object.assign({}, obj)
+      _obj.name = name.trim()
+      result.push(_obj)
+    }
+  }
+
+  return result
 }
 
 module.exports = Sanitizer
